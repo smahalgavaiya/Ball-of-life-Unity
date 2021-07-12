09c6ba7ea830d44abb005b093998a968c7d7bf93
smahalgavaiya <36693350+smahalgavaiya@users.noreply.github.com>
2021-07-12 09:59:28 +0530

    brute anims added and code implimented

--- Assets/scripts/generateMinions.cs
+++ Assets/scripts/generateMinions.cs
@@ -6,10 +6,11 @@ public class generateMinions : MonoBehaviour
 {
     public GameObject minionsPrefab;
     public Transform parent;
+    public int generateCount = 10;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; ++i)
+        for (int i = 0; i < generateCount; ++i)
         {
             GameObject min=GameObject.Instantiate(minionsPrefab, parent);
             min.transform.position=new Vector2(parent.transform.position.x+i * 1f, parent.transform.position.y + i * 1f);
